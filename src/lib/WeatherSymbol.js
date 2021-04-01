8038193ee5cd266842103091676bc504750339bd
jensbankel <jenban1204@skola.goteborg.se>
2021-04-01 15:03:32 +0200

    added isNight() in weatherSymbol

--- src/lib/WeatherSymbol.js
+++ src/lib/WeatherSymbol.js
@@ -1,10 +1,26 @@
 function setWeatherSymbol(Wsymb2) {
     let imgUrl = ""
+    function isNight() {
+        let hour = new Date().getHours()
+        console.log(hour)
+        if (hour > 20 || hour < 6) {
+            return true
+        }
+        return false
+    }
 
     if (Wsymb2 <= 2) {
-        imgUrl = require("../assets/sun.png")
+        if (isNight()) {
+            imgUrl = require("../assets/clear_night.png")
+        } else {
+            imgUrl = require("../assets/sun.png")
+        }
     } else if (Wsymb2 <= 4) {
-        imgUrl = require("../assets/sun_cloudy.png")
+        if (isNight()) {
+            imgUrl = require("../assets/cloudy_night.png")
+        } else {
+            imgUrl = require("../assets/sun_cloudy.png")
+        }
     } else if (Wsymb2 <= 7) {
         imgUrl = require("../assets/cloudy.png")
     } else if (Wsymb2 <= 9 || Wsymb2 == 18 || Wsymb2 == 19) {
