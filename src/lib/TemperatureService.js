d71674192b53d301b5332c1cc0da81b3fed799bc
henrikbuller <henbul0928@goteborg.skola.se>
2021-04-08 10:48:47 +0200

    created convert function that creates a set of date objects

--- src/lib/TemperatureService.js
+++ src/lib/TemperatureService.js
@@ -1,4 +1,10 @@
 const BASE_URL = "https://opendata-download-metfcst.smhi.se/api"
+// import getDayOfYear from "date-fns/getDayOfYear"
+// import { sv } from "date-fns/locale"
+import getMonth from "date-fns/getMonth"
+import getDate from "date-fns/getDate"
+import format from "date-fns/format"
+import parseJSON from "date-fns/parseJSON"
 
 // const gbg = { name: "Göteborg", lng: 16.158, lat: 58.5812 }
 
@@ -7,6 +13,8 @@ const TemperatureService = {
         const url = `${BASE_URL}/category/pmp3g/version/2/geotype/point/lon/${city.lng}/lat/${city.lat}/data.json`
         const response = await fetch(url)
         const forecast = await response.json()
+
+        convert(forecast)
 
         const currentTemp = findTemperature(forecast.timeSeries[hour].parameters)
         console.log("current temp inside service async method:", currentTemp)
@@ -18,6 +26,28 @@ const TemperatureService = {
         }
     },
 }
+
+function convert(forecast) {
+    let currentDateData = new Set()
+
+    for (const data of forecast.timeSeries) {
+        data.validTime = parseJSON(data.validTime)
+        const thisDate = {
+            name: format(data.validTime, "eeee"),
+            date: getDate(data.validTime),
+            month: format(getMonth(data.validTime), "MMM"),
+        }
+        currentDateData.add(thisDate)
+    }
+    console.log("Logging convert function")
+    console.log("currentDateData:")
+    console.log(currentDateData)
+    console.log("forecast.timeseries:")
+    console.log(forecast.timeseries)
+}
+
+// function matchDate(forecast, dateFromCarousel) {}
+
 function findTemperature(parameters) {
     for (const param of parameters) {
         if (param.name === "t") {
