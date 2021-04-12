67c1b27322648203be390b29e44a08cc3254b875
jensbankel <jenban1204@skola.goteborg.se>
2021-04-12 22:11:35 +0200

    removed <p> se väder i....

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,6 @@
     <div class="home">
         <navbar /><br />
         <img src="../assets/swedenMap.png" height="500" width="187" alt="sweden map" /><br /><br />
-        <p>Se vädret i</p>
         <menu-component class="box" />
         <button class="box" type="button" @click="$router.push('sun')">Nästa</button>
     </div>
